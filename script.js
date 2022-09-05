4a50ddda689adc7962a39fc141fc752395d2c876
Ben Iverson <2513iverson989@gmail.com>
2022-09-05 14:02:43 -0500

    countChar functions as intended

--- script.js
+++ script.js
@@ -8,4 +8,12 @@ function countBs(str) {
   return count;
 }
 
-function countChar(str, char) {}
+function countChar(str, char) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === char) {
+      count += 1;
+    }
+  }
+  return count;
+}
