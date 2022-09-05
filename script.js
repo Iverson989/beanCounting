6c6272712bc4821ba88e862d82604d1e8a0e7920
Ben Iverson <2513iverson989@gmail.com>
2022-09-05 13:59:27 -0500

    countBs function finished

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function countBs(str) {}
+function countBs(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "B") {
+      count += 1;
+    }
+  }
+  return count;
+}
 
 function countChar(str, char) {}
